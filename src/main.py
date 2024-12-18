6dd544d46c91fe95b043a3ed5ab212afb587dac7
VIILing <uvyke@outlook.com>
2024-12-18 19:43:22 +0800

    use dynamic import insted manually import

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
-from init import get_app, register_all
-from routes.bilibili import bilibili_main  # Use dynamic import instead of manual import in the future
+import os
+import importlib
+from init import get_app, register_all, get_logger
+
+
+logger = get_logger()
+
+
+routes_dir = os.path.join(os.path.dirname(__file__), 'routes')
+
+for root, dirs, files in os.walk(routes_dir):
+    for dir_name in dirs:
+        main_file = os.path.join(root, dir_name, 'main.py')
+        if os.path.isfile(main_file):
+            module_name = f"routes.{dir_name}.main"
+            importlib.import_module(module_name)
+            logger.info(f"Successfully imported {module_name}")
 
 
 app = get_app()
