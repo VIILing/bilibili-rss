3871f0dfdd6a180e7ddd87a535409fca89b8a665
VIILing <uvyke@outlook.com>
2024-12-29 03:42:19 +0800

    change build shell

--- build_image.sh
+++ build_image.sh
@@ -5,4 +5,4 @@ script_path=$(realpath "$0")
 dir_path=$(dirname "$script_path")
 
 commitId=$(git rev-parse --short HEAD)
-sudo docker build -t VIILing/bilibili-rss:"$commitId" -t VIILing/bilibili-rss:latest "$dir_path"
+sudo docker build -t VIILing/bilibili-rss:"$commitId" -t VIILing/bilibili-rss:dev "$dir_path"
